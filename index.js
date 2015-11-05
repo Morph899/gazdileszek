8ef76f42320c097eaf79f793f4e9d7a7252af9e7
mate.kertesz <mate.kertesz@performgroup.com>
2015-11-05 13:35:08 +0100

    Edited on impl

--- index.js
+++ index.js
@@ -4,14 +4,32 @@ var koa = require("koa"),
 	path = require("path"),
 	views = require("koa-views"),
 	config = require("config"),
-	serve = require("koa-static");
+	serve = require("koa-static"),
+	mongoose = require("mongoose"),
+	db = null,
+	app = null;
 
 
-var app = module.exports = koa();
+app = module.exports = koa();
 
 // initialize render helper
 app.use(views(config.template.path, config.template.options));
 app.use(serve('.'));
 require('./app/routes')(app);
 
+
 if (!module.parent) app.listen(3000);
+
+function initDB() {
+	mongoose.connect("mongodb://localhost/gazdileszek");
+	db = mongoose.connection;
+
+	db.on('error', function(){
+		console.log("Error on connection to db");
+	});
+	db.once('open', function() {
+		console.log("Connected to db");
+	});
+}
+
+initDB();
